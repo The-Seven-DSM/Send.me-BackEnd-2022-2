4c07889457225284aa70aabdaee4f72ad3bfa12a
caio <caiovitordias1@gmail.com>
2022-10-02 20:38:10 -0300

    mudanca na variavel port para atender ao heroku e nos dados do user padrao

--- SendMeApp.ts
+++ SendMeApp.ts
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 import db from "./src/config/Database.config";
 import route from "./src/routes/routes";
-const port = 3001;
+const port =  process.env.PORT || 3001;
 
 
 app.use(cors())
--- src/config/Database.config.ts
+++ src/config/Database.config.ts
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize('API_a', 'root', 'admin',{
+const db = new Sequelize('api_a', 'adminUser', 'admin',{
     host: 'localhost',
     dialect:'mysql'
 });
